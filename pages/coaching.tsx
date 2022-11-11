bb39450877203feba6db96db121588d2c8ec0aba
Oleg Chursin <olegchursin@gmail.com>
2022-11-11 17:53:16 -0500

    fix: Refactor coaching view

--- pages/coaching.tsx
+++ pages/coaching.tsx
@@ -1,27 +1,21 @@
+import BannerHero from '../components/elements/BannerHero';
 import CoachingCta from '../components/elements/CoachingCta';
 import Layout from '../components/layout/Layout';
 
 function Coaching() {
   return (
-    <>
-      <Layout>
-        <section className="section-box">
-          <div className="banner-hero banner-breadcrums">
-            <div className="container text-center">
-              <h1 className="text-heading-2 color-gray-1000 mb-20">Коучинг</h1>
-              <p className="text-body-text color-gray-500">
-                Профессиоанльный индивидуальный коучинг
-              </p>
-            </div>
-          </div>
-        </section>
+    <Layout>
+      <section className="section-box">
+        <BannerHero
+          title="Коучинг"
+          subtitle="Профессиоанльный индивидуальный коучинг"
+        />
+      </section>
 
-        <section className="section-box">
-          <CoachingCta />
-        </section>
-      </Layout>
-    </>
+      <section className="section-box">
+        <CoachingCta />
+      </section>
+    </Layout>
   );
 }
-
 export default Coaching;
