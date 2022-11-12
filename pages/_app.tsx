c6f261873fe990f5b71a505da2efbb5e0e94ad7f
Oleg Chursin <olegchursin@gmail.com>
2022-11-11 21:38:21 -0500

    fix: Remove wowjs dep

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,24 +1,9 @@
-import Preloader from '../components/elements/Preloader';
-import React, { useEffect, useState } from 'react';
 import '../public/assets/css/app.2afad0c.bundle.css';
 import '../public/assets/css/swiper-custom.css';
 import 'react-modal-video/css/modal-video.css';
 
 function MyApp({ Component, pageProps }) {
-  const [loading, setLoading] = useState(false);
-
-  useEffect(() => {
-    setLoading(true);
-    setTimeout(() => {
-      setLoading(false);
-    }, 2000);
-
-    const isServer = typeof window === 'undefined';
-    const WOW = !isServer ? require('wowjs').WOW : null;
-
-    new WOW().init();
-  }, []);
-  return <>{!loading ? <Component {...pageProps} /> : <Preloader />}</>;
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
