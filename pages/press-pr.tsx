de69d138a50224203a7efbc8e45ed8b8f89e29b3
Oleg Chursin <olegchursin@gmail.com>
2022-11-13 14:09:32 -0500

    F - Add new section placeholders

---
+++ pages/press-pr.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const PressPr = () => {
+  return <div>PR в СМИ</div>;
+};
+
+export default PressPr;
--- util/routes.ts
+++ util/routes.ts
@@ -1,17 +1,23 @@
 export const HOME_ROUTE = '/';
 export const ABOUT_US_ROUTE = '/about';
-export const ABOUT_US_PUBLICATIONS_ROUTE = '/about/publications';
 export const CONTACT_US_ROUTE = '/contact';
+export const PRESS_PR = '/press-pr';
+export const PR = '/pr';
+export const PR_SPORT = '/pr/sport';
+export const PR_FASHION = '/pr/fashion';
+export const PR_PERSONAL_BRAND = '/pr/personal-brand';
+export const PR_PSYCHOLOGY = '/pr/psychology';
 export const TRAINING_ROUTE = '/training';
 export const TRAINING_INTERVIEW_ROUTE = '/training/interview';
 export const TRAINING_JOURNALISM_ROUTE = '/training/journalism';
 export const TRAINING_SPORTSPR_CLUB_ROUTE = '/training/sportspr-club';
 export const TRAINING_SPORTSPR_PACKAGE_ROUTE = '/training/sportspr-package';
-export const TRAINING_SPORTSPR_INDIVIDUAL_ROUTE =
-  '/training/sportspr-individual';
-export const COACHING_ROUTE = '/coaching';
-export const SERVICES_ROUTE = '/services';
+export const TRAINING_SPORTSPR_INDIVIDUAL_ROUTE = '/training/sportspr-individual';
 export const BLOG_ROUTE = '/blog';
+
+// deprecated
+export const SERVICES_ROUTE = '/services';
+export const COACHING_ROUTE = '/coaching';
 
 export interface NavLink {
   readonly id: string;
@@ -24,34 +30,44 @@ export const navLinks: NavLink[] = [
   {
     id: 'about',
     label: 'О нас',
+    href: ABOUT_US_ROUTE
+  },
+  {
+    id: 'pr',
+    label: 'PR',
     href: '#',
     children: [
       {
-        id: 'about',
-        label: 'Кто мы',
-        href: ABOUT_US_ROUTE
+        id: 'pr-sport',
+        label: 'Спорт',
+        href: PR_SPORT
       },
       {
-        id: 'publications',
-        label: 'Наши работы',
-        href: ABOUT_US_PUBLICATIONS_ROUTE
+        id: 'pr-fashion',
+        label: 'Fashion',
+        href: PR_FASHION
+      },
+      {
+        id: 'pr-personal-brand',
+        label: 'Личный бренд',
+        href: PR_PERSONAL_BRAND
+      },
+      {
+        id: 'pr-psychology',
+        label: 'Психология',
+        href: PR_PSYCHOLOGY
       }
     ]
   },
   {
-    id: 'services',
-    label: 'Услуги',
-    href: SERVICES_ROUTE
+    id: 'press-pr',
+    label: 'PR в СМИ',
+    href: PRESS_PR
   },
   {
     id: 'training',
     label: 'Тренинги',
     href: TRAINING_ROUTE
-  },
-  {
-    id: 'coaching',
-    label: 'Коучинг',
-    href: COACHING_ROUTE
   },
   {
     id: 'contact',
