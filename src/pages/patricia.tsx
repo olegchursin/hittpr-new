1f2ea6316ff8b497dbc6d497302bebde6b4a7539
Oleg Chursin <olegchursin@gmail.com>
2023-07-12 18:58:14 -0400

    feat: add patricia links

--- src/pages/patricia.tsx
+++ src/pages/patricia.tsx
@@ -11,6 +11,7 @@ const case_item = {
   title: 'Patricia Field',
   subtitle: 'Материалы',
   category: { label: 'Lifestyle', href: '/pr/fashion' },
+  renderMaterials: false,
   materials: [
     {
       id: 'two',
@@ -43,6 +44,15 @@ const case_item = {
       desc: '',
       url: 'https://www.glamour.ru/fashion-news/ya-hochu-priehat-v-rossiyu-i-potusovatsya-s-vami-hudozhnik-po-kostyumam-i-stilist-patriciya-fild'
     }
+  ],
+  links: [
+    'https://www.glamour.ru/fashion-news/ya-hochu-priehat-v-rossiyu-i-potusovatsya-s-vami-hudozhnik-po-kostyumam-i-stilist-patriciya-fild',
+    'https://www.kiz.ru/content/stil-zhizni/svobodnoe-vremya/patrishiya-fild-moy-garderob-eto-edinyy-orkestr/',
+    'https://hothitnewyork.com/patricia-field/',
+    'https://hothitnewyork.com/pat-in-the-city/',
+    'https://youtu.be/Jd9LVVhne5A',
+    'https://trello.com/1/cards/642efe60cac441bacae41daa/attachments/645375cdb3156a49eb0d4a18/download/PATRICIA_FIELD__THE_FASHIONABLE_TOPIC_OF_FEMINISM_DOES_NOT_TAKE_UP_MY_HEAD_SPACE_(Rabotnitsa_Magazine).docx',
+    'https://trello.com/1/cards/642efe60cac441bacae41daa/attachments/645375ce4cd2d66bc953120f/download/Beuty_and_Health_-_Patricia_Field_article_translated.docx'
   ],
   description:
     ' Патришия Филд (Patricia Field)  – американский художник по костюмам, стилист, модный дизайнер, лауреат двух премий “Эмми” и номинант на “Оскар”. Широкая публика ассоциирует ее с такими сериалами, как «Секс в большом городе” (Sex and the city), «Эмили в Париже» (Emily in Paris) и кинокартиной “Дьявол носит Prada” (The Devil Wears Prada).'
