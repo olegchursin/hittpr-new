16cdfb5f1dfcfa8132c44bbe16294570f407b861
Oleg Chursin <olegchursin@gmail.com>
2023-09-24 22:58:12 -0400

    fix(content): FIx /doctors content

--- src/pages/doctors.tsx
+++ src/pages/doctors.tsx
@@ -13,47 +13,45 @@ const case_item = {
   category: { label: 'Services', href: '/pr/services' },
   sectionedLinks: [
     {
-      sectionTitle: 'Ада Кондэ',
-      sectionSubtitle:
-        'Мотивационный спикер, основатель проекта об осознанном управлении жизнью',
+      sectionTitle: 'Вилор Шпитальник',
+      sectionSubtitle: 'Психиатр, врач высшей американской категории.',
       sectionLinks: [
-        'https://hothitnewyork.com/ada-konde/',
-        'https://woman.forumdaily.com/schaste-s-inostrancem-preodolevaya-to-chto-ne-stoit-preodolevat/',
-        'https://www.thevoicemag.ru/lifestyle/stil-zhizni/v-poiskah-svoego-lica-odin-na-odin-s-dermatilomaniey/',
-        'https://www.psychologies.ru/articles/dermatilomaniya-kak-ugomonit-ruki/'
+        'https://www.psychologies.ru/articles/leto-samyiy-stressovyiy-sezon/',
+        'https://hothitnewyork.com/psychiatrist_vilor_shpitalnik/'
       ],
       sectionPdfs: [
         {
-          title: 'Дерматиломания: как угомонить руки',
-          url: 'https://drive.google.com/file/d/1QVT-LxM7t9js9F57sQeEShqSuzIUyV4a/view?usp=sharing'
+          title: 'Женская ненависть — путь к фригидности',
+          url: 'https://drive.google.com/file/d/1DqpcY8wZhb1nEAX48tKYx-jqob-sTFk8/view?usp=sharing'
+        },
+        {
+          title: 'Как реагировать на эксгибициониста?',
+          url: 'https://drive.google.com/file/d/1u5oYrmvHsv7kPnbxJr1NvW9pBk2I0YNe/view?usp=sharing'
+        }
+      ]
+    },
+    {
+      sectionTitle: 'Дарья Патракеева',
+      sectionSubtitle: 'Врач-офтальмолог',
+      sectionLinks: [
+        'https://www.7ya.ru/article/Kak-nosit-kontaktnye-linzy-pravilno-7-voprosov-oftalmologu/#ocenka',
+        'https://www.kiz.ru/content/zdorove/novosti/6-mifov-o-kontaktnykh-linzakh/'
+      ],
+      sectionPdfs: [
+        {
+          title: 'Макияж: безопасность для глаз',
+          url: 'https://drive.google.com/file/d/1BQ7eoFpt5IQzz4WI8jZgGINA7r0Ebash/view?usp=sharing'
         }
       ]
     }
   ],
   links: [
-    'https://hothitnewyork.com/psychiatrist_vilor_shpitalnik/',
-    'https://www.psychologies.ru/articles/leto-samyiy-stressovyiy-sezon/',
     'https://woman.forumdaily.com/kompleks-kotoryj-byvaet-u-vsex-kak-ne-slomat-sudbu-rebenku/',
     'https://woman.forumdaily.com/pereezd-v-ssha-kak-pomoch-rebenku-adaptirovatsya/',
     'https://woman.forumdaily.com/pereezd-v-ssha-i-problemy-adaptacii-chto-ozhidat-i-kak-spravitsya/',
-    'https://woman.forumdaily.com/kak-adaptiruyutsya-v-ssha-pozhilye-roditeli/',
-    'https://www.kiz.ru/content/zdorove/novosti/6-mifov-o-kontaktnykh-linzakh/',
-    'https://www.7ya.ru/article/Kak-nosit-kontaktnye-linzy-pravilno-7-voprosov-oftalmologu/#ocenka'
+    'https://woman.forumdaily.com/kak-adaptiruyutsya-v-ssha-pozhilye-roditeli/'
   ],
-  pdfs: [
-    {
-      title: 'Женская ненависть — путь к фригидности',
-      url: 'https://drive.google.com/file/d/1DqpcY8wZhb1nEAX48tKYx-jqob-sTFk8/view?usp=sharing'
-    },
-    {
-      title: 'Как реагировать на эксгибициониста?',
-      url: 'https://drive.google.com/file/d/1u5oYrmvHsv7kPnbxJr1NvW9pBk2I0YNe/view?usp=sharing'
-    },
-    {
-      title: 'Макияж: безопасность для глаз',
-      url: 'https://drive.google.com/file/d/1BQ7eoFpt5IQzz4WI8jZgGINA7r0Ebash/view?usp=sharing'
-    }
-  ],
+  pdfs: [],
   renderMaterials: false,
   materials: [],
   description: 'Врачи'
