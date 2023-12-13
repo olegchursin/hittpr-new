bd14baf8b746681d6eb12d96155826eb6d38ce75
Oleg Chursin <olegchursin@gmail.com>
2023-12-12 21:35:18 -0500

    fix: package updates

--- src/components/seo.tsx
+++ src/components/seo.tsx
@@ -7,21 +7,19 @@ interface SEOProps {
 }
 
 const SEO = ({ pageTitle, font }: SEOProps) => (
-  <>
-    <Head>
-      <title>{pageTitle && `${pageTitle} | HITTPR`}</title>
-      <meta httpEquiv="x-ua-compatible" content="ie=edge" />
-      <meta name="description" content="Продвижение бизнеса в новых условиях" />
-      <meta name="robots" content="noindex, follow" />
-      <meta
-        name="viewport"
-        content="width=device-width, initial-scale=1, shrink-to-fit=no"
-      />
+  <Head>
+    <title>{pageTitle && `${pageTitle} | HITTPR`}</title>
+    <meta httpEquiv="x-ua-compatible" content="ie=edge" />
+    <meta name="description" content="Продвижение бизнеса в новых условиях" />
+    <meta name="robots" content="noindex, follow" />
+    <meta
+      name="viewport"
+      content="width=device-width, initial-scale=1, shrink-to-fit=no"
+    />
 
-      {font && <Link href={font} rel="stylesheet" />}
-      <Link rel="icon" href="/favicon.ico" />
-    </Head>
-  </>
+    {font && <Link href={font} rel="stylesheet" />}
+    <Link rel="icon" href="/favicon.ico" />
+  </Head>
 );
 
 export default SEO;
